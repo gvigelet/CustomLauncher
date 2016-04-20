18501f4486a4a1cbceed331c982724e17ade3ac7
George Vigelette <gvigelet@duvitech.com>
2016-04-20 01:06:07 -0400

    Initial Commit

---
+++ app/src/main/java/com/duvitech/customlauncher/GameEntity.java
@@ -0,0 +1,14 @@
+package com.duvitech.customlauncher;
+
+/**
+ * Created by George on 4/20/2016.
+ */
+public class GameEntity {
+    public int imageResId;
+    public int titleResId;
+
+    public GameEntity (int imageResId, int titleResId){
+        this.imageResId = imageResId;
+        this.titleResId = titleResId;
+    }
+}
